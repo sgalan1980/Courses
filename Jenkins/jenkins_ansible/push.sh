85ce25258c43b954357d7b686be1cd3d5d149624
sgalan1980 <sgs_23@hotmail.com>
2019-01-28 23:08:50 +0100

    Jenkins_20190128

--- Jenkins/jenkins_ansible/push.sh
+++ Jenkins/jenkins_ansible/push.sh
@@ -8,7 +8,7 @@ while [ $counter -lt 50 ]; do
     apellido=$(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $2}')
     edad=$(shuf -i 20-25 -n 1)
 
-    mysql -u root -p1234 people -e "insert into registro values ('$counter', '$name', '$apellido', '$edad')"
+    mysql -u root  people -e "insert into registro values ('$counter', '$name', '$apellido', '$edad')"
     echo "$counter, $name, $apellido, $edad se importaron correctamente"
     sleep 7
 
